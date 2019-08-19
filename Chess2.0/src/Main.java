62336dc04df82dd3e31495e5d1f53301e018524f
Jessica Eugster <jessica.eugster@gmail.ch>
2019-08-19 20:37:33 +0200

    created Scene java.fx

--- Chess2.0/src/Main.java
+++ Chess2.0/src/Main.java
@@ -1,24 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import model.Board;
 import model.Coordinate;
 
-public class Main {
+public class Main extends Application {
 
 	public static void main(String[] args) {
-		Board board = new Board();
-		board.movePiece(new Coordinate(0,0), new Coordinate(0,0));
-		board.movePiece(new Coordinate(0,0),new Coordinate(0,6));
-		board.movePiece(new Coordinate(0,6),new Coordinate(0,7));
-		
-		board.movePiece(new Coordinate(1,1),new Coordinate(1,0));
-		board.movePiece(new Coordinate(1,1),new Coordinate(1,1));
-		board.movePiece(new Coordinate(1,1),new Coordinate(2,1));
-		board.movePiece(new Coordinate(1,1),new Coordinate(1,2));
-		
-		board.movePiece(new Coordinate(3,0),new Coordinate(3,1));
-		board.movePiece(new Coordinate(3,1),new Coordinate(4,1));
-		board.movePiece(new Coordinate(4,1),new Coordinate(3,0));
-		board.movePiece(new Coordinate(3,0),new Coordinate(5,1));
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource(""));
+		
+		Scene scene = new Scene(root,400,400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 
 }
---
+++ Chess2.0/src/controller/ChessViewController.java
@@ -0,0 +1,11 @@
+package controller;
+
+
+
+public class ChessViewController {
+	
+	public void initialize() throws Exception {
+
+	}
+
+}
